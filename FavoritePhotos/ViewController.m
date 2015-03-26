c1d5202441af00004760e00be0db52a47fd5f155
jenkelley <i.jen.kelley@gmail.com>
2015-03-26 16:01:23 -0500

    storyboard set

--- FavoritePhotos/ViewController.m
+++ FavoritePhotos/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
 
@@ -16,12 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
